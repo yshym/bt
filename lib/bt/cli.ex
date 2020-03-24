e9ad81f35c33bb8fdb1f641a468cf456b0de5a6d
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-03-24 17:35:56 +0200

    Add response parsing

--- lib/bt/cli.ex
+++ lib/bt/cli.ex
@@ -17,8 +17,45 @@ defmodule Bt.CLI do
     argument :device
 
     run context do
-      System.cmd("bluetoothctl", ["connect", context.device])
+      {_res, code} = System.cmd("bluetoothctl", ["connect", context.device])
+      if code == 0 do
+        IO.puts("Device was successfully connected")
+      else
+        IO.puts("Failed to connect")
+      end
     end
+  end
+
+  command :disconnect do
+    aliases [:dcon]
+    description "Disconnect device"
+    long_description """
+    Disconnect bluetooth device
+    """
+
+    argument :device
+
+    run context do
+      {_res, code} = System.cmd("bluetoothctl", ["disconnect", context.device])
+      if code == 0 do
+        IO.puts("Device was successfully disconnected")
+      else
+        IO.puts("Failed to disconnect")
+      end
+    end
+  end
+
+  def parse_list(list) do
+    list
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.reduce(
+        [],
+        fn x, acc ->
+          [_, mac, name] = x |> String.split(" ", parts: 3)
+          acc ++ [%{} |> Map.put(:mac, mac) |> Map.put(:name, name)]
+        end
+      )
   end
 
   command :devices do
@@ -30,12 +67,20 @@ defmodule Bt.CLI do
 
     run context do
       {res, _code} = System.cmd("bluetoothctl", ["devices"])
-      devices =
-        res
-        |> String.trim()
-        |> String.split("\n")
-        |> Enum.map(fn x -> x |> String.split(" ", parts: 3) |> Enum.at(2) end)
-      IO.puts(devices)
+      res |> parse_list() |> IO.inspect()
+    end
+  end
+
+  command :adapters do
+    aliases [:controllers]
+    description "List adapters"
+    long_description """
+    List bluetooth adapters
+    """
+
+    run context do
+      {res, _code} = System.cmd("bluetoothctl", ["list"])
+      res |> parse_list() |> IO.inspect()
     end
   end
 end
