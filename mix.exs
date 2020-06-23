b954a69acbf7275fca8f2c81beee04e62f164cb7
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-06-24 00:58:45 +0300

    Update project version to 0.2.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Bt.MixProject do
   def project do
     [
       app: @app,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       escript: escript(),
