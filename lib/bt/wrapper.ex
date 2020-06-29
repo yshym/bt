fe12e622297a04cdca72781f60e1aaca0bd37a95
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-06-29 17:00:29 +0300

    Add docs for functions of Wrapper module

--- lib/bt/wrapper.ex
+++ lib/bt/wrapper.ex
@@ -3,6 +3,9 @@ defmodule Bt.Wrapper do
   Provides some useful functional wrappers
   """
 
+  @doc """
+  Return exit code 1 if function exits
+  """
   @spec with_exit_code(fun) :: integer
   def with_exit_code(f) do
     try do
